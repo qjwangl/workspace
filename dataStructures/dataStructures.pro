4bb6cfbc7256a96248dca61807514f1bcbfde438
qjwangl <qjwanglll@gmail.com>
2018-11-12 03:05:05 -0800

    sort.cpp

--- dataStructures/dataStructures.pro
+++ dataStructures/dataStructures.pro
@@ -4,9 +4,5 @@ CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += main.cpp \
-<<<<<<< HEAD
     parameter.cpp \
     sort.cpp
-=======
-    parameter.cpp
->>>>>>> e81902883494efb2eeb85618083c2283538d84ea
--- dataStructures/sort.cpp
+++ dataStructures/sort.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 using namespace std;
+
+void array_test(int a[], int n)
+{
+    //int n = sizeof(a)/sizeof(int);//sizoef(a)的结果是指针变量a占内存的大小
+    for(int i = 0; i < n; i++)
+    {
+        cout << a[i] << endl;
+    }
+    return ;
+}
+
 /*
  *冒泡排序为交换排序，每完成一轮交换，后位排序，先在为排序的序列中找到到最大的进行排序
  *冒泡排序时间复杂度为O(n^2),
@@ -35,6 +46,7 @@ void bubbleSort(int a[],int n)
 
 /*
  * 插入排序对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+ * 时间复杂度为O(n^2)
  */
 
 void insertionSort(int a[], int n)
@@ -61,22 +73,63 @@ void insertionSort(int a[], int n)
             cout << "for" << j << endl;
         }
         cout << j << endl;
-        a[j+1] = value;//插入数据
+        a[j+1] = value;//插入数据,j--做了-1的操作
 
     }
 
     return ;
 }
 
-void array_test(int a[], int n)
+/*
+ * 从未排序的序列中找到最小的，进行交换
+ * 时间复杂度为O(n^2)
+ */
+void selectionSort(int a[], int n)
 {
-    //int n = sizeof(a)/sizeof(int);//sizoef(a)的结果是指针变量a占内存的大小
-    for(int i = 0; i < n; i++)
+    if(n <= 1)
     {
-        cout << a[i] << endl;
+        return ;
+    }
+    int minIndex = 0;
+    int tmp = 0;
+    for(int i = 0; i < n - 1; i++)//执行n-1次，
+    {
+        minIndex = i;
+        for(int j = i + 1; j < n; j++)
+        {
+            if(a[j] < a[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        tmp = a[i];
+        a[i] = a[minIndex];
+        a[minIndex] = tmp;
+
     }
     return ;
 }
+
+
+
+void mergeSort_c(int a[], int begin, int end)
+{
+    if(begin >= end)
+    {
+        return ;
+    }
+
+    int middle = ( begin + end ) / 2;
+    mergeSort_c(a, begin, middle);
+    mergeSort_c(a, middle + 1, end);
+}
+
+void mergeSort(int a[], int n)
+{
+    mergeSort_c(a, 0, n - 1);
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -84,8 +137,9 @@ int main(int argc, char *argv[])
 
     int n = sizeof(a)/sizeof(int);
     //bubbleSort(a, n);
-
-    insertionSort(a, n);
-    array_test(a, n);
+    //insertionSort(a, n);
+    //selectionSort(a, n);
+    mergeSort(a,n);
+    //array_test(a, n);
     return 0;
 }
