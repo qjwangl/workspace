9f97eae502a1174fc8976dfd970b5d2d54565dba
qjwangl <qjwanglll@gmail.com>
2018-11-13 03:02:44 -0800

    sort.cpp

--- dataStructures/sort.cpp
+++ dataStructures/sort.cpp
@@ -12,6 +12,8 @@ void array_test(int a[], int n)
     return ;
 }
 
+
+
 /*
  *冒泡排序为交换排序，每完成一轮交换，后位排序，先在为排序的序列中找到到最大的进行排序
  *冒泡排序时间复杂度为O(n^2),
@@ -110,18 +112,55 @@ void selectionSort(int a[], int n)
     return ;
 }
 
-
-
-void mergeSort_c(int a[], int begin, int end)
+void merge(int arr[],int l,int mid,int r)
 {
-    if(begin >= end)
+    int aux[r-l+1];//开辟一个新的数组，将原数组映射进去
+    for(int m=l;m<=r;m++)
+    {
+        aux[m-l]=arr[m];
+    }
+    cout << "-----------------------" << endl;
+    array_test(aux,sizeof(aux)/sizeof(int));
+    cout << "-----------------------" << endl;
+
+    int i=l,j=mid+1;//i和j分别指向两个子数组开头部分
+
+    for(int k=l;k<=r;k++)
+    {
+        if(i>mid)
+        {
+            arr[k]=aux[j-l];
+            j++;
+        }
+        else if(j>r)
+        {
+            arr[k]=aux[i-l];
+            i++;
+        }
+        else if(aux[i-l]<aux[j-l])
+        {
+            arr[k]=aux[i-l];
+            i++;
+        }
+        else
+        {
+            arr[k]=aux[j-l];
+            j++;
+        }
+    }
+}
+
+void mergeSort_c(int a[], int l, int r)
+{
+    if(l >= r)
     {
         return ;
     }
-
-    int middle = ( begin + end ) / 2;
-    mergeSort_c(a, begin, middle);
-    mergeSort_c(a, middle + 1, end);
+    int mid = ( l + r ) / 2;
+    mergeSort_c(a, l, mid);
+    mergeSort_c(a, mid + 1, r);
+    cout << "l = " <<l << "mid = " << mid << "r = " << r << endl;
+    merge(a,l,mid,r);
 }
 
 void mergeSort(int a[], int n)
@@ -131,15 +170,57 @@ void mergeSort(int a[], int n)
 
 
 
+/*
+ *快速排序
+ */
+//实现函数，用于partition的递归
+int partitionSort(int arr[], int l ,int r)//l是数组最左边，r为最右边
+{
+    int j = l;//设计标记
+    int t = arr[l];//设置主元
+
+    for(int i = l+1; i <= r; i++)
+    {
+        if(arr[i] < t)
+        {
+            //cout << arr[j+1] <<"swap" <<arr[i] << endl;
+            swap(arr[j+1],arr[i]);
+            j++;
+        }
+    }
+
+    //cout << "arr[l]=" << arr[l] << ",arr[j]=" << arr[j] << ",j = " << j << endl;
+    swap(arr[l],arr[j]);
+    return j;
+}
+
+void partition(int arr[], int l, int r)
+{
+    if(l >= r)
+    {
+        return ;
+    }
+    int p = partitionSort(arr,l,r);
+    cout << "p = " << p << endl;
+    partition(arr,l,p-1);//0-1 p=2
+    partition(arr,p+1,r);//3-5
+}
+
+void quickSort(int arr[],int n)
+{
+    partition(arr,0,n-1);
+}
+
 int main(int argc, char *argv[])
 {
-    int a[] = {1,3,5,2,4,6};
+    int a[] = {3,5,1,2,4,6};
 
     int n = sizeof(a)/sizeof(int);
     //bubbleSort(a, n);
     //insertionSort(a, n);
     //selectionSort(a, n);
-    mergeSort(a,n);
-    //array_test(a, n);
+    //mergeSort(a,n);
+    quickSort(a, n);
+    array_test(a, n);
     return 0;
 }
